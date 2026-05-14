672baf52cc1fb5e2ac30d045df204b2b4e729b3c
JereTaylor <jtaylo22@umbc.edu>
2026-05-14 11:01:06 -0400

    Update under_works_not_tested.ino

--- ICS_CC/Autonomous_Follower/under_works_not_tested.ino
+++ ICS_CC/Autonomous_Follower/under_works_not_tested.ino
@@ -299,7 +299,7 @@ void getDesiredPolar() {
 
   //desiredHeading = atan2(dx, dy) * 180.0 / PI;
   if (desiredHeading < 0) {
-    desiredHeading -= 360.0;
+    desiredHeading += 360.0;
   }
   desiredHeading = -(desiredHeading - 90.0);
   if (desiredHeading < 0) {

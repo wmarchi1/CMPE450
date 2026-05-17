14df695ad8b522f5e6573e61133c88acab4e75eb
Muhammad Umar <115104242+umar-mah@users.noreply.github.com>
2026-05-17 19:24:41 -0400

    Add receiver functionality for data packets

---
+++ ICS_CC/Convoy_Ops/rx_lead.ino
@@ -0,0 +1,62 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 6);  // CE, CSN
+const byte address[6] = "00100";
+
+float x_;
+float y_;
+float heading_;
+float speed_;
+bool e_stop_;
+
+struct DataPacket {
+  float x;
+  float y;
+  float heading;
+  float speed;
+  bool e_stop;
+};
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Receiver Start");
+
+  SPI.begin();
+  delay(1000);
+  radio.begin(&SPI);
+  delay(1000);
+
+  radio.openReadingPipe(1, address);
+  delay(1000);
+  radio.setPALevel(RF24_PA_MIN);
+  delay(1000);
+  radio.startListening();
+  Serial.println("Receiver Ready");
+}
+
+void loop() {
+  while (radio.available()) {
+    DataPacket pkt;
+    radio.read(&pkt, sizeof(pkt));
+
+    x_ = pkt.x;
+    y_ = pkt.y;
+    heading_ = pkt.heading;
+    speed_ = pkt.speed;
+    e_stop_ = pkt.e_stop;
+
+    Serial.print(x_);
+    Serial.print(",");
+    Serial.print(y_);
+    Serial.print(",");
+    Serial.print(heading_);
+    Serial.print(",");
+    Serial.print(speed_);
+    Serial.print(",");
+    Serial.println(e_stop_);
+  }
+}
